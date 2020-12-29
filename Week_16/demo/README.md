ee82aa8fb670d7e5b7f23e0c206c0e09ba43cc4a
TAROTAORAN <tianruo.tian@hand-china.com>
2020-12-29 18:11:58 +0800

    add: Weel_16

---
+++ Week_16/demo/README.md
@@ -0,0 +1,6 @@
+#### 学习笔记  
+##### 工具链    
+* 初始化构建 | 初始化工具Yeoman  
+  - yo vue测试文件夹： 
+  1. 执行yo vue
+  2. 执行webpack(目前copy-webpack-plugin会报错)
---
+++ Week_16/demo/src/main.js
@@ -0,0 +1,6 @@
+import HelloWord from './HelloWord.vue';
+import Vue from "vue";
+new Vue({
+    el: "#app",
+    render: h => h(HelloWord),
+});
